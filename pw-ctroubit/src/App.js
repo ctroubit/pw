0230a08fcb3ffecbae7c557167cf2960d5fa5821
ctroubit <ctroubit@uwo.ca>
2024-09-09 18:00:45 -0400

    Added picture carousel to home page

--- pw-ctroubit/src/App.js
+++ pw-ctroubit/src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Home from "./components/Home";
 import {
     BrowserRouter as Router,
     Routes,
     Route,
 } from "react-router-dom";
 
-import Home from "./pages/Home";
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Header/>
         <h1 className="bruh">pick your favourite!</h1>
         <Navbar></Navbar>
-        <About></About>
+       <Home/>
         </>
         
     );
--- pw-ctroubit/src/components/About.js
+++ pw-ctroubit/src/components/About.js
@@ -3,7 +3,7 @@ import "./About.css"
 const About = () =>{
     return(
         <div class="AboutMain">
-            hello!
+            
         </div>
     );
 }
